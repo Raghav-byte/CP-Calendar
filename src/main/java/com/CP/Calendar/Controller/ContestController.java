3a197d1b73881bbda7f63401a993d8d6a1d35a5a
Raghav  Shukla <55020567+Raghav-byte@users.noreply.github.com>
2025-01-07 23:50:14 +0530

    Update ContestController.java

--- src/main/java/com/CP/Calendar/Controller/ContestController.java
+++ src/main/java/com/CP/Calendar/Controller/ContestController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin("*")
 @RequestMapping("/contest")
 @RestController
 public class ContestController {
