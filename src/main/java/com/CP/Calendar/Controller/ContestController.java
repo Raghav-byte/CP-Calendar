0d23094a07a64d622095a8689231ac881c0f597e
Raghav  Shukla <55020567+Raghav-byte@users.noreply.github.com>
2025-01-07 23:54:45 +0530

    Update ContestController.java

--- src/main/java/com/CP/Calendar/Controller/ContestController.java
+++ src/main/java/com/CP/Calendar/Controller/ContestController.java
@@ -6,10 +6,7 @@ import com.CP.Calendar.Service.ContestService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
